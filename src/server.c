e547ad7f5072349156a330db3f4b60c3dd959ed3
Tamsen1995 <Tamanhbui95@gmail.com>
2019-10-28 17:47:22 -0700

    [UPD]

--- src/server.c
+++ src/server.c
@@ -12,12 +12,16 @@ int create_server(int port)
 	struct protoent *proto;
 	struct sockaddr_in sin;
 
+	ft_putendl("chingdao$>1:  ");
 	proto = getprotobyname("tcp");
 	if (proto == 0)
 		return (-1);
 	sock = socket(PF_INET, SOCK_STREAM, proto->p_proto);
+	ft_putendl("chingdao$>2:  ");
 	sin.sin_family = AF_INET;
+	ft_putendl("chingdao$>4:  ");
 	sin.sin_port = htons(port);
+	ft_putendl("chingdao$>3:  ");
 	sin.sin_addr.s_addr = htonl(INADDR_ANY);
 	// bind the socket config so to speak to the socket in question
 	if (bind(sock, (const struct sockaddr *)&sin, sizeof(sin)))
@@ -44,13 +48,17 @@ int main(int ac, char **av)
 		usage(av[1]);
 	port = ft_atoi(av[1]);
 	sock = create_server(port);
+	ft_putendl("chingdao$>5:  ");
 	cs = accept(sock, (struct sockaddr *)&csin, &cslen);
+	// RIGHT HERE IS WHERE THE CLIENT CONNECTS
+	// SO IDEALLY WE WOULD FORK() HERE
 	while ((r = read(cs, buf, 1023)) > 0)
 	{
 		buf[r] = '\0';
 		printf("received %d bytes: [%s]\n", r, buf);
 	}
 	close(cs);
+
 	close(sock);
 	return (0);
 }
