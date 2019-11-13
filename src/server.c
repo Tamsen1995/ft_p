73c7c70e74ce3b11b0bd26ba2be5277e7519da75
Tamsen1995 <Tamanhbui95@gmail.com>
2019-11-12 21:14:08 -0800

    [UPD] The server now creates the directory and also switches its own location into said directory

--- src/server.c
+++ src/server.c
@@ -10,21 +10,24 @@ void make_server_directory()
 {
 	char buf[1024];
 	int r;
-	const char *tmp;
-	const char *aux;
+	char *tmp;
+	char *aux;
 
 	getcwd(buf, 1023);
-	aux = ft_strdup("/sever_dir");
+	aux = ft_strdup("/server_dir");
 	tmp = ft_strjoin((const char *)buf, aux);
 	r = mkdir(tmp, S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
-	if (r == 0)
+	if (r == 0 && chdir(tmp) == 0)
+	{
+		free(tmp);
+		free(aux);
 		return;
+	}
 	else if (r == -1)
 	{
+		ft_putendl("An error occurred whilst making a server directory");
 		exit(-1);
 	}
-	free(tmp);
-	free(aux);
 }
 
 int create_server(int port)
@@ -37,11 +40,8 @@ int create_server(int port)
 	if (proto == 0)
 		return (-1);
 	sock = socket(PF_INET, SOCK_STREAM, proto->p_proto);
-
 	sin.sin_family = AF_INET;
-
 	sin.sin_port = htons(port);
-
 	sin.sin_addr.s_addr = htonl(INADDR_ANY);
 	// bind the socket config so to speak to the socket in question
 	if (bind(sock, (const struct sockaddr *)&sin, sizeof(sin)))
